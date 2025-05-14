d2f203ac23df40ff8af7a84e20a89e934b9da06c
WARPSTAR <alpay.gueruez@hsrw.org>
2025-05-14 16:52:11 +0200

    Delete sterne.js

--- sterne.js
+++
@@ -1,9 +0,0 @@
-function stern(){
-    var line = document.getElementById("display").innerHTML;
-    if(line === "..."){
-        line = "";
-    }
-    line += " &#9733";
-    document.getElementById("display").innerHTML = line;
-    console.log(line);
-}
